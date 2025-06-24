a3f6b028726428c69b2e03e5da464a40cfe25dcb
Masa <forpracticemasaya@gmail.com>
2025-06-24 18:28:24 +0200

    Fix German header translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,16 +4,16 @@ import en from './locales/en.json'
 import es from './locales/es.json'
 import de from './locales/de.json'
 
-const savedLocale = localStorage.getItem('lang')
+const defaultLocale = 'en'
+
+let savedLocale = defaultLocale
+if (typeof window !== 'undefined') {
+  savedLocale = localStorage.getItem('lang') || defaultLocale
+}
 
 export const i18n = createI18n({
   legacy: false,
-  locale: savedLocale || 'en', // 英語をデフォルトに
-  fallbackLocale: 'en',
-  messages: {
-    ja,
-    en,
-    es,
-    de,
-  },
+  locale: savedLocale,
+  fallbackLocale: defaultLocale,
+  messages: { ja, en, es, de },
 })
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import './assets/tailwind.css'
 import { i18n } from './i18n'
 
 const app = createApp(App)
+
 app.use(router)
 app.use(i18n)
+
 app.mount('#app')
