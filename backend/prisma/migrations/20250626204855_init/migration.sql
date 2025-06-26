a3e1383445af3ac609feda46f92848c7c2bda590
Masa <forpracticemasaya@gmail.com>
2025-06-26 22:51:29 +0200

    fix: update CORS settings and switch DB to PostgreSQL

---
+++ backend/prisma/migrations/20250626204855_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
